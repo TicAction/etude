09e6b439aa6232cca5a6bf75bc85092b3bd4979d
Ghislain Girard <ghischa@gmail.com>
2020-09-20 14:06:11 -0400

    section main terminer

---
+++ resources/views/main.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+@section('content')
+<br>
+<br>
+<h1 class="text-center">Bienvenue sur le site de la classe de M. Ghislain</h1>
+
+<h3 class="text-center">Vous trouverez sur le site les leçons et les devoirs de la semaine.</h3>
+
+<br>
+<br>
+<div class="container text-center">
+    <div class="row align-items-center h-100">
+        <div class="col-6 mx-auto">
+          <a href="#"><button class="btn btn-primary">Groupe 341</button></a>
+          <a href="#"><button class="btn btn-primary">Groupe 431</button></a>
+        </div>
+    </div>
+</div>
+@endsection
